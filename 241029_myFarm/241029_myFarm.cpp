6f21cc096e03f747e9d707ab640f8c74ed725805
KiimEviL <kimasia3000@gmail.com>
2024-10-29 11:07:55 +0700

    Добавьте файлы проекта.

---
+++ 241029_myFarm/241029_myFarm.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Fruit {
+private:
+	string mName;
+public:
+	Fruit(const string& name = "");
+	string getName() const;
+};
+
+Fruit::Fruit(const string& name) :mName(name) {};
+string Fruit::getName() const {
+	return mName;
+}
+
+class Farm {
+private:
+	vector<Fruit> mFruits;
+public:
+	Farm();
+	void add(const Fruit& c);
+	void allFruits();
+};
+Farm::Farm() {
+	cout << "Farm created!" << endl;
+}
+void Farm::add(const Fruit& c) {
+	mFruits.push_back(c);
+}
+
+void Farm::allFruits() {
+	for (vector<Fruit>::iterator iter = mFruits.begin(); iter != mFruits.end(); iter++) {
+		cout << iter->getName() << endl;
+	}
+}
+
+int main() {
+	Fruit apple("apple");
+	cout << apple.getName() << endl;
+	Farm f;
+	f.add(Fruit("pineapple"));
+	f.add(Fruit("melon"));
+	f.allFruits();
+	return 0;
+}
